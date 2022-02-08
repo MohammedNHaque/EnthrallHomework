bb4fae0ca5bf1717533e33f266eb264ab98cba4c
Mohammed Haque <mohammedhaque@macbook-pro.mynetworksettings.com>
2022-02-07 22:45:39 -0500

    02/07/22 10:45PM

---
+++ src/hw1Q2JavaBasics/MySelf.java
@@ -0,0 +1,14 @@
+package hw1Q2JavaBasics;
+/*
+ *Naming convention :
+ *		Project name starts with upper case
+ *		Package name starts with lower case
+ *		Class name starts with upper case
+ */
+public class MySelf {
+	// The main access to the Java
+		public static void main(String[] args) {
+			System.out.println("Mohammed N Haque");
+			
+}
+}
